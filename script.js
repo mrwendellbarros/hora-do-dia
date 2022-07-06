b83eebdb6b7956d446020525438c0eabdb7d5591
Wendell Gabriel Fernandes Barros <102330689+mrwendellbarros@users.noreply.github.com>
2022-07-06 12:17:05 -0300

    alteração

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ function carregar() {
   var hora = data.getHours()
   msg.innerHTML = `Agora são ${hora} horas.`
   if ( hora >= 0 && hora < 12) {
-    img.src = 'fotomanha.png'
+    img.src = './fotomanha.png'
     document.body.style.background = '#ddbf81'
   } else if (hora >= 12 && hora <= 18) {
-    img.src = 'fototarde.png'
+    img.src = './fototarde.png'
     document.body.style.background = '#bc856b'
   } else {
-    img.src = 'fotonoite.png'
+    img.src = './fotonoite.png'
     document.body.style.background = '#3380a0'
   }
 }
